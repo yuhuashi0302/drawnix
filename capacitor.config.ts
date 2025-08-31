9e33da900fca196bbb3ea565a67937c711ad6d2b
yuhuashi0302 <yuhuashi0302@163.com>
2025-08-31 13:48:09 +0800

    fix: build web assets before sync ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.buguniaotest',
   appName: 'DrawNix',
-  webDir: 'dist'
+  webDir: 'dist/apps/web'
 };
 
 export default config;
