fb12c7406b4d30020da09a131a7b372285b1e603
yuhuashi0302 <yuhuashi0302@163.com>
2025-08-31 11:22:17 +0800

    feat(ios): add generated ios platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.buguniaotest',
+  appName: 'DrawNix',
+  webDir: 'dist'
+};
+
+export default config;
